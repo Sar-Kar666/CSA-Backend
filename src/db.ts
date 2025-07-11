4bb1e0ec572962c7e9c8453362b2b5c2b61129e6
Sar-Kar666 <diptanusarkar699@gmail.com>
2025-07-12 02:24:50 +0530

    fixing

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,7 @@
 import {model,Schema} from "mongoose";
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://diptanusarkar3020:taq2YRLqI3CJIGhW@cluster12.jqdns3v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster12")
- .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("Connection error:", err));
+mongoose.connect("mongodb+srv://diptanusarkar3020:taq2YRLqI3CJIGhW@cluster12.jqdns3v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster12");
 
 const UserSchema = new Schema({
     email: String,
