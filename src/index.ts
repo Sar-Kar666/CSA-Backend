a70b0c6610fdc3326e15b7e197cac7b6c3b44cc3
Sar-Kar666 <diptanusarkar699@gmail.com>
2025-07-25 03:17:23 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const app= express();
 app.use(express.json());
 app.use(cors({
-  origin: ["https://course-selling-website-delta.vercel.app"],
+  origin: ["https://course-selling-website-delta.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST"],
   credentials: true
 }))
